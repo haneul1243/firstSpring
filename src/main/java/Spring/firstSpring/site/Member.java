bff0d93dda1e2868a7b814f7e6ff49a07930ad58
haneul <yoonhyun0128@gmail.com>
2023-09-21 23:21:25 +0900

    초기 파일

---
+++ src/main/java/Spring/firstSpring/site/Member.java
@@ -0,0 +1,22 @@
+package Spring.firstSpring.site;
+
+public class Member {
+  private Long Id;
+  private String Name;
+
+  public Long getId() {
+    return Id;
+  }
+
+  public void setId(Long id) {
+    Id = id;
+  }
+
+  public String getName() {
+    return Name;
+  }
+
+  public void setName(String name) {
+    Name = name;
+  }
+}
